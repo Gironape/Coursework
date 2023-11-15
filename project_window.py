f25444408e08b1b7e747f9a3fdd3ba7da65c941e
Gironape <andreyka_kazakov_03@mail.ru>
2023-11-15 22:14:27 +0400

    работа...

--- project_window.py
+++ project_window.py
@@ -1,6 +1,8 @@
 import random
 import sys
 
+from PyQt5.QtPrintSupport import QPrinter
+
 import tools as tl
 from PyQt5 import QtCore
 from PyQt5.QtCore import *
@@ -24,6 +26,10 @@ class Project(QMainWindow):
         self.create_menu_bar()
         self.create_tool_bar()
         self.setAcceptDrops(True)
+        self.scene = QGraphicsScene(self)
+        self.view = QGraphicsView(self.scene)
+        self.setCentralWidget(self.view)
+        self.item = QGraphicsItem()
 
     def buttons(self):
         self.btn_exit = QPushButton(self)
@@ -90,10 +96,31 @@ class Project(QMainWindow):
         print("Action: " + action.text())
 
     def tool_bar_clicked(self, btn):
-        if btn.text() == "Battery":
-            battery = QPushButton('Battery', self)
-            battery.move(random.randint(0, 300), random.randint(0, 180))
-            battery.show()
-            bat = tl.Tools()
-            bat.show()
+        if btn.text() == "Resistor":
+            resistor = self.scene.addRect(50, 50, 100, 50)
+            resistor = self.scene.addLine(150, 75, 200, 75)
+            resistor = self.scene.addLine(0, 75, 50, 75)
+            self.scene.addItem(resistor)
+        elif btn.text() == "Lamp":
+            lamp = self.scene.addEllipse(0, 0, 100, 100)
+            lamp = self.scene.addLine(100, 50, 150, 50)
+            lamp = self.scene.addLine(-50, 50, 0, 50)
+            lamp = self.scene.addLine(20, 10, 80, 90)
+            lamp = self.scene.addLine(80, 10, 20, 90)
+            self.scene.addItem(lamp)
+        elif btn.text() == "Key":
+            key = self.scene.addLine(50, 50, 100, 50)
+            key = self.scene.addLine(100, 50, 150, 20)
+            key = self.scene.addLine(150, 50, 200, 50)
+            self.scene.addItem(key)
+        elif btn.text() == "Battery":
+            battery = self.scene.addLine(50, 50, 100, 50)
+            battery = self.scene.addLine(100, 25, 100, 75)
+            battery = self.scene.addLine(125, 0, 125, 100)
+            battery = self.scene.addLine(125, 50, 175, 50)
+            battery = self.scene.addLine(75, 40, 90, 40)
+            battery = self.scene.addLine(130, 40, 145, 40)
+            battery = self.scene.addLine(137.5, 33, 137.5, 47)
+            self.scene.addItem(battery)
+
 
