7d9de3c1c1873db927042833a5e9e8a7c2488a95
Gironape <andreyka_kazakov_03@mail.ru>
2023-11-20 09:53:32 +0400

    Работа
    work...

--- project_window.py
+++ project_window.py
@@ -1,10 +1,7 @@
-import random
 import sys
 
-from PyQt5.QtPrintSupport import QPrinter
-
 import tools as tl
-from PyQt5 import QtCore
+from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
@@ -26,10 +23,11 @@ class Project(QMainWindow):
         self.create_menu_bar()
         self.create_tool_bar()
         self.setAcceptDrops(True)
-        self.scene = QGraphicsScene(self)
+        self.scene = Scene()
         self.view = QGraphicsView(self.scene)
         self.setCentralWidget(self.view)
-        self.item = QGraphicsItem()
+        self.view.mouseDoubleClickEvent = self.mouseDoubleClickEvent
+
 
     def buttons(self):
         self.btn_exit = QPushButton(self)
@@ -76,14 +74,12 @@ class Project(QMainWindow):
         # tools.setShortcut('Ctrl+Q') Нужно для понимания
         # tools.triggered.connect(qApp.quit)
         key = QAction(QIcon('key.png'), 'Key', self)
-        resistor = QAction(QIcon('resistor.png'), 'Resistor', self)
         lamp = QAction(QIcon('lamp.png'), 'Lamp', self)
         wire = QAction(QIcon('wire.png'), 'Wire', self)
 
         self.toolbar = self.addToolBar('tools')
         self.toolbar.addAction(battery)
         self.toolbar.addAction(key)
-        self.toolbar.addAction(resistor)
         self.toolbar.addAction(lamp)
         self.toolbar.addAction(wire)
         self.toolbar.setFixedHeight(100)
@@ -96,31 +92,163 @@ class Project(QMainWindow):
         print("Action: " + action.text())
 
     def tool_bar_clicked(self, btn):
-        if btn.text() == "Resistor":
-            resistor = self.scene.addRect(50, 50, 100, 50)
-            resistor = self.scene.addLine(150, 75, 200, 75)
-            resistor = self.scene.addLine(0, 75, 50, 75)
-            self.scene.addItem(resistor)
-        elif btn.text() == "Lamp":
-            lamp = self.scene.addEllipse(0, 0, 100, 100)
-            lamp = self.scene.addLine(100, 50, 150, 50)
-            lamp = self.scene.addLine(-50, 50, 0, 50)
-            lamp = self.scene.addLine(20, 10, 80, 90)
-            lamp = self.scene.addLine(80, 10, 20, 90)
-            self.scene.addItem(lamp)
+        if btn.text() == "Lamp":
+            lamp = QPixmap('Lamp.png')
+            pixmap_item = QGraphicsPixmapItem(lamp.scaled(60, 60))
+            pixmap_item.setFlag(QGraphicsItem.ItemIsMovable)
+            self.scene.addItem(pixmap_item)
         elif btn.text() == "Key":
-            key = self.scene.addLine(50, 50, 100, 50)
-            key = self.scene.addLine(100, 50, 150, 20)
-            key = self.scene.addLine(150, 50, 200, 50)
-            self.scene.addItem(key)
+            key = QPixmap('Key.png')
+            pixmap_item = QGraphicsPixmapItem(key.scaled(60, 60))
+            pixmap_item.setFlag(QGraphicsItem.ItemIsMovable)
+            self.scene.addItem(pixmap_item)
         elif btn.text() == "Battery":
-            battery = self.scene.addLine(50, 50, 100, 50)
-            battery = self.scene.addLine(100, 25, 100, 75)
-            battery = self.scene.addLine(125, 0, 125, 100)
-            battery = self.scene.addLine(125, 50, 175, 50)
-            battery = self.scene.addLine(75, 40, 90, 40)
-            battery = self.scene.addLine(130, 40, 145, 40)
-            battery = self.scene.addLine(137.5, 33, 137.5, 47)
-            self.scene.addItem(battery)
+            battery = QPixmap('Battery.png')
+            pixmap_item = CustomItem(battery.scaled(60, 60))
+            pixmap_item.setFlag(QGraphicsItem.ItemIsMovable)
+            self.scene.addItem(pixmap_item)
+
+
+class Scene(QtWidgets.QGraphicsScene):
+    startItem = newConnection = None
+
+    def controlPointAt(self, pos):
+        mask = QPainterPath()
+        mask.setFillRule(QtCore.Qt.WindingFill)
+        for item in self.items(pos):
+            if mask.contains(pos):
+                # ignore objects hidden by others
+                return
+            if isinstance(item, ControlPoint):
+                return item
+            if not isinstance(item, ControlPoint):
+                mask.addPath(item.shape().translated(item.scenePos()))
+
+    def mousePressEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton:
+            item = self.controlPointAt(event.scenePos())
+            if item:
+                self.startItem = item
+                self.newConnection = Connection(item, event.scenePos())
+                self.addItem(self.newConnection)
+                return
+        super().mousePressEvent(event)
+
+    def mouseMoveEvent(self, event):
+        if self.newConnection:
+            item = self.controlPointAt(event.scenePos())
+            if (item and item != self.startItem and
+                self.startItem.onLeft != item.onLeft):
+                    p2 = item.scenePos()
+            else:
+                p2 = event.scenePos()
+            self.newConnection.setP2(p2)
+            return
+        super().mouseMoveEvent(event)
+
+    def mouseReleaseEvent(self, event):
+        if self.newConnection:
+            item = self.controlPointAt(event.scenePos())
+            if item and item != self.startItem:
+                self.newConnection.setEnd(item)
+                if self.startItem.addLine(self.newConnection):
+                    item.addLine(self.newConnection)
+                else:
+                    # delete the connection if it exists; remove the following
+                    # line if this feature is not required
+                    self.startItem.removeLine(self.newConnection)
+                    self.removeItem(self.newConnection)
+            else:
+                self.removeItem(self.newConnection)
+        self.startItem = self.newConnection = None
+        super().mouseReleaseEvent(event)
+
+
+class Connection(QtWidgets.QGraphicsLineItem):
+    def __init__(self, start, p2):
+        super().__init__()
+        self.start = start
+        self.end = None
+        self._line = QtCore.QLineF(start.scenePos(), p2)
+        self.setLine(self._line)
+
+    def controlPoints(self):
+        return self.start, self.end
+
+    def setP2(self, p2):
+        self._line.setP2(p2)
+        self.setLine(self._line)
+
+    def setStart(self, start):
+        self.start = start
+        self.updateLine()
+
+    def setEnd(self, end):
+        self.end = end
+        self.updateLine(end)
+
+    def updateLine(self, source):
+        if source == self.start:
+            self._line.setP1(source.scenePos())
+        else:
+            self._line.setP2(source.scenePos())
+        self.setLine(self._line)
+
+
+class ControlPoint(QtWidgets.QGraphicsEllipseItem):
+    def __init__(self, parent, onLeft):
+        super().__init__(-5, -5, 10, 10, parent)
+        self.onLeft = onLeft
+        self.lines = []
+        # this flag **must** be set after creating self.lines!
+        self.setFlags(self.ItemSendsScenePositionChanges)
+
+    def addLine(self, lineItem):
+        for existing in self.lines:
+            if existing.controlPoints() == lineItem.controlPoints():
+                # another line with the same control points already exists
+                return False
+        self.lines.append(lineItem)
+        return True
+
+    def removeLine(self, lineItem):
+        for existing in self.lines:
+            if existing.controlPoints() == lineItem.controlPoints():
+                self.scene().removeItem(existing)
+                self.lines.remove(existing)
+                return True
+        return False
+
+    def itemChange(self, change, value):
+        for line in self.lines:
+            line.updateLine(self)
+        return super().itemChange(change, value)
+
+
+class CustomItem(QtWidgets.QGraphicsPixmapItem):
+    def __init__(self, pixmap, left=True, right=True, parent=None):
+        super().__init__(pixmap, parent)
+
+        self.setFlags(self.ItemIsMovable)
+
+        self.controls = []
+
+        for onLeft, create in enumerate((right, left)):
+            if onLeft == right:
+                control = ControlPoint(self, onLeft)
+                self.controls.append(control)
+                control.setPen(QPen(QtCore.Qt.red, 2))
+                control.setBrush(QBrush(QColor(214, 13, 36)))
+                if onLeft:
+                    control.setX(pixmap.width())
+                control.setY(pixmap.height() / 2)
+            else:
+                control = ControlPoint(self, onLeft)
+                self.controls.append(control)
+                control.setPen(QPen(QtCore.Qt.blue, 2))
+                control.setBrush(QBrush(QColor(00, 00, 255)))
+                if onLeft:
+                    control.setX(pixmap.width())
+                control.setY(pixmap.height() / 2)
 
 
--- tools.py
+++ tools.py
@@ -1,19 +1,47 @@
-import sys
-
-from PyQt5 import QtCore
-from PyQt5.QtCore import QSize
+from PyQt5.QtCore import QRectF, QLineF
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
 
 
-class Tools(QWidget):
+class Lamp(QGraphicsItem):
     def __init__(self):
-        super(Tools, self).__init__()
-        self.setGeometry(500, 200, 100, 100)
-        self.v = None
-        self.battery()
+        super().__init__()
 
-    def battery(self):
-        self.v = QLabel(self)
-        self.v.setText("Напряжение 220В")
-        self.show()
+    def boundingRect(self):
+        return QRectF(0, 0, 100, 100)
+
+    def paint(self, painter, option, widget):
+        painter.drawEllipse(0, 0, 100, 100)
+        painter.drawLine(QLineF(100, 50, 150, 50))
+        painter.drawLine(QLineF(-50, 50, 0, 50))
+        painter.drawLine(QLineF(20, 10, 80, 90))
+        painter.drawLine(QLineF(80, 10, 20, 90))
+
+
+class Key(QGraphicsItem):
+    def __init__(self):
+        super().__init__()
+
+    def boundingRect(self):
+        return QRectF(0, 0, 150, 50)
+
+    def paint(self, painter, option, widget):
+        painter.drawLine(QLineF(0, 0, 50, 0))
+        painter.drawLine(QLineF(50, 0, 100, -30))
+        painter.drawLine(QLineF(100, 0, 150, 0))
+
+
+class Battery(QGraphicsItem):
+    def __init__(self):
+        super().__init__()
+
+    def boundingRect(self):
+        return QRectF(0, 0, 125, 100)
+
+    def paint(self, painter, option, widget):
+        painter.drawLine(QLineF(50, 50, 100, 50))
+        painter.drawLine(QLineF(100, 25, 100, 75))
+        painter.drawLine(QLineF(125, 0, 125, 100))
+        painter.drawLine(QLineF(125, 50, 175, 50))
+        painter.drawLine(QLineF(75, 40, 90, 40))
+        painter.drawLine(QLineF(130, 40, 145, 40))
+        painter.drawLine(QLineF(137.5, 33, 137.5, 47))
